8f40930e8e023bf330cfbde5aa2447a1d0fe7713
yonziii_ <mryon19072017@gmail.com>
2025-05-26 21:29:19 +0700

    feat(flashcard): done

--- script.js
+++ script.js
@@ -21,3 +21,29 @@ toggle.addEventListener('click', () => {
     }
 });
 
+//card flipped
+const card = document.querySelector('.card');
+function flipCard() {
+    card.classList.toggle('flipped');
+    if (card.classList.contains('flipped')) {
+        showAnswerBtn.textContent = 'Show Question';
+    } else {
+        showAnswerBtn.textContent = 'Show Answer';
+    }
+}
+card.addEventListener('click', flipCard);
+
+document.addEventListener('DOMContentLoaded', function() {
+    const showAllBtn = document.getElementById('show-all-btn');
+    const allCards = document.getElementById('all-cards');
+
+    showAllBtn.addEventListener('click', function(){
+
+        // Show all cards
+        allCards.style.display = 'block';
+        
+        // Hide the button itself
+        showAllBtn.style.display = 'none';
+    });
+});
+
